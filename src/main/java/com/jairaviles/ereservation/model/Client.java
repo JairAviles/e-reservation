473af79e3c55281cb0bd52d83c6e45b642f79821
Jair Avilés <hi@jairaviles.mx>
2019-10-19 21:59:53 -0700

    Add entities definition Add application file definition Add new dependencies

---
+++ src/main/java/com/jairaviles/ereservation/model/Client.java
@@ -0,0 +1,27 @@
+package com.jairaviles.ereservation.model;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy="uuid2")
+    private String idClient;
+    private String first;
+    private String last;
+    private String username;
+    private String address;
+    private String phone;
+    private String email;
+
+    @OneToMany(mappedBy = "client")
+    private Set<Reservation> reservations;
+
+}
---
+++ src/main/java/com/jairaviles/ereservation/model/Reservation.java
@@ -0,0 +1,24 @@
+package com.jairaviles.ereservation.model;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "reservation")
+public class Reservation {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy="uuid2")
+    private String idReservation;
+    private Date startDate;
+    private Date endDate;
+    private int totalPeople;
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Client client;
+}
