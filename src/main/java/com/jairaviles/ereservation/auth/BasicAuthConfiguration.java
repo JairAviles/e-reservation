e4b0a07d841941a17a59d4df588d982855a29dd5
Jair Avilés <hi@jairaviles.mx>
2019-10-20 21:24:36 -0700

    Add docker plugin definition

--- src/main/java/com/jairaviles/ereservation/auth/BasicAuthConfiguration.java
+++ src/main/java/com/jairaviles/ereservation/auth/BasicAuthConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class BasicAuthConfiguration extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
